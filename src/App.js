009efbbd12fed32ddeb25e9d488f0396989df431
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-03-02 20:51:09 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1>Full Name Display</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <p>First Name:</p>
+          <input type='text' value={firstName} onChange={(e) => setfirstName(e.target.value)} />
+        </div>
+        <div>
+          <p>Last Name:</p>
+          <input type='text' value={lastName} onChange={(e) => setlastName(e.target.value)} />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+      {submitted ? <p>Full Name: {firstName} {lastName}</p>: <p></p>}
     </div>
   );
 }
